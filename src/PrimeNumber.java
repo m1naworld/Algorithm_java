aee02f43f41a2d71e01b101cd5b234b40b889c74
mina <m1nna@naver.com>
2022-11-21 17:53:25 +0900

    프로그래머스: 소수 만들기

---
+++ src/PrimeNumber.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrimeNumber {
+
+    public static int solution(int[] nums) {
+
+        // 기존 내 코드 >> 평균 5.x ms 
+        boolean[] prime = new boolean[3000];
+
+        // 소수 판별
+        Outter:
+        for (int i = 2; i < prime.length; i++) {
+            for (int j = 1; j < i; j++) {
+                if (prime[j] == true) {
+                    if (i % j == 0) {
+                        continue Outter;
+                    }
+                }
+            }
+            prime[i] = true;
+        }
+
+        // 범준님 코드  >> 평균 1.00ms 이하
+//        boolean[] prime = new boolean[3000+1];
+//        Arrays.fill(prime, true); // 소수 배열 초기화
+//        prime[0] = false;
+//        prime[1] = false;
+//
+//        for(int i=2; i <= 3000; i++){
+//            if(prime[i]){
+//                for(int l = i+i; i <= 3000; l+=i){
+//                    prime[l] = false;
+//                }
+//                if(i*i >= 3000)break;
+//            }
+//        }
+
+
+        int primeCount = 0;
+        int numsLength = nums.length;
+
+        for (int k = 0; k < numsLength; k++) {
+            for (int l = k + 1; l < numsLength; l++) {
+                for (int m = l + 1; m < numsLength; m++) {
+                    int number = nums[k] + nums[l] + nums[m];
+                    if (prime[number] == true) {
+                        primeCount += 1;
+                    }
+                }
+            }
+        }
+        System.out.println(primeCount);
+        return primeCount;
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 7, 6, 4};
+        int[] b = {1, 2, 3, 4};
+
+        solution(a);
+        solution(b);
+    }
+}
