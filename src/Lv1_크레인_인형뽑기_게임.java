c2e904f0007a480af006263864e924e16586411e
mina <m1nna@naver.com>
2022-11-26 00:28:28 +0900

    프로그래머스: 크레인 인형뽑기 게임

--- src/Lv1_크레인_인형뽑기_게임.java
+++ src/Lv1_크레인_인형뽑기_게임.java
@@ -4,29 +4,31 @@ public class Lv1_크레인_인형뽑기_게임 {
 
     public static int solution(int[][] board, int[] moves) {
 
-        // board 배열 > n * n
+        // n * n 배열
         int boardLength = board.length;
 
-        // 뽑아온 값 저장 리스트
+        // 뽑아 인형 담는 바구니
         ArrayList<Integer> answer = new ArrayList<>();
 
-        // 각 위치별 어디까지 뽑았는지 알아내는 인덱스 j 저장 배열
+        // 왼쪽 바구니의 위치별 인형이 있는 칸을 저정하는 배열
         int[] counts = new int[boardLength + 1];
 
         // 주어진 배열 for 문
         for (int i = 0; i < moves.length; i++) {
 
-            // 타깃 위치
+            // 왼쪽 바구니 위치
             int target = moves[i];
 
-
+            // 왼쪽 바구니 인형이 있는 경우부터
             for (int j = counts[target]; j < board.length; j++) {
 
-                // 꺼내는 위치에 값이 없는 경우
+                // 꺼내는 위치에 인형이 없는 경우
                 if (board[j][target - 1] == 0) {
-                    counts[target] += 1;
+                    counts[target] += 1; // 인형있는 위치 찾기
                     continue;
                 }
+
+                // 오른쪽 바구니에 저장
                 answer.add(board[j][target - 1]);
                 counts[target] += 1;
                 break;
@@ -34,13 +36,16 @@ public class Lv1_크레인_인형뽑기_게임 {
             }
         }
 
+        // 뽑은 인형들 중 중복된 경우 제거
         int result = 0;
         for (int i = 0; i < answer.size() - 1; ) {
             if (answer.get(i).equals(answer.get(i + 1))) {
                 result += 2;
                 answer.remove(i + 1);
                 answer.remove(i);
-                i = 0;
+                if (i != 0) {
+                    i -= 1;
+                }
             } else {
                 i += 1;
             }
@@ -54,10 +59,14 @@ public class Lv1_크레인_인형뽑기_게임 {
 //        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4 , 5} ;
 //        solution(board, moves);
 
-        int[][] board1 = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-        int[] moves1 = {1, 2, 3, 4};
 
-        solution(board1, moves1);
+//        int[][] board1 = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+//        int[] moves1 = {1, 2, 3, 4};
+//        solution(board1, moves1);
+//
+        int[][] board2 = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {1, 1, 2, 2, 3}};
+        int[] moves2 = {1, 2, 3};
+        solution(board2, moves2);
 
     }
 }
